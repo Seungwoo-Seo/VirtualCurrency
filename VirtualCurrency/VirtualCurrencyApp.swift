c2698fb5b4cbbd0588d3b681e86c659a6166e214
Seungwoo-Seo <prodvssw.98@gmail.com>
2024-01-30 16:18:09 +0900

    Initial Commit

---
+++ VirtualCurrency/VirtualCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualCurrencyApp.swift
+//  VirtualCurrency
+//
+//  Created by 서승우 on 2024/01/30.
+//
+
+import SwiftUI
+
+@main
+struct VirtualCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VirtualCurrencyUITests/VirtualCurrencyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VirtualCurrencyUITestsLaunchTests.swift
+//  VirtualCurrencyUITests
+//
+//  Created by 서승우 on 2024/01/30.
+//
+
+import XCTest
+
+final class VirtualCurrencyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
